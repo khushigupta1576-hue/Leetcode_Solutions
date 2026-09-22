ac16c810a5436893068ebf516f38985a3b8a2966
khushigupta1576-hue <khushigupta1576@gmail.com>
2026-09-22 11:03:48 +0530

    Time: 3 ms (5.53%), Space: 47.9 MB (96.57%) - LeetHub-Neo

---
+++ 0344-reverse-string/0344-reverse-string.java
@@ -0,0 +1,17 @@
+class Solution {
+    public void reverseString(char[] s) {
+        int start=0, end=s.length-1;
+        while(start<end)
+	    {
+		 char temp=s[start];
+		 s[start]=s[end];
+		 s[end]=temp;
+		 
+		 start++;
+		 end--;
+        }
+        String r = new String(s);
+        System.out.println(r);
+    }
+}
+    
